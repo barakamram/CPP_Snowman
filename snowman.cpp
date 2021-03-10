eb0debd0a113c13e64633e5ed2e9c88d49154b33
barakamram <74139212+barakamram@users.noreply.github.com>
2021-03-10 03:59:47 +0200

    Add files via upload

---
+++ snowman.cpp
@@ -0,0 +1,11 @@
+#include "snowman.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+namespace ariel{
+    string snowman (int input){
+        return "  //_\\ \n (-_-)\n <(> <)> \n (   ) ";
+    }
+}
